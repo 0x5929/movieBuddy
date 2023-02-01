6efd6ff7e3c9b5a979fa26f3a5f1620bf4dac06e
rennitbaby <work.kevin.ren@gmail.com>
2023-02-01 04:24:08 -0800

    first project commit, installed tailwindcss

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1 class="text-3xl font-bold text-black underline bg-green-300">
+      App injected with tailwindcss :)
+    </h1>
+  </main>
+</template>
